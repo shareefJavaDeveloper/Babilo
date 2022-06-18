8460246dd1697a7021191ed8afa6bc0cf3de81e0
PulaNikhila <npula@mgmresorts.com>
2022-06-18 02:03:58 -0500

    front end initial commit

--- src/main/java/com/screens/babilo/dao/MovieBookingDao.java
+++
@@ -1,7 +0,0 @@
-package com.screens.babilo.dao;
-
-import com.screens.babilo.dataentity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieBookingDao extends JpaRepository<Ticket,Integer> {
-}
--- src/main/java/com/screens/babilo/dao/MovieDao.java
+++
@@ -1,7 +0,0 @@
-package com.screens.babilo.dao;
-
-import com.screens.babilo.dataentity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieDao extends JpaRepository<Movie,Integer> {
-}
